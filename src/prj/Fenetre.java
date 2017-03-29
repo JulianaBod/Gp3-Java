87126c40a99e9e42b4060fcf9c9dcfa0be548981
DavSCo <david.cohen@ynov.com>
2017-03-29 12:09:42 +0200

    fenetre

--- src/prj/Fenetre.java
+++ src/prj/Fenetre.java
@@ -1,65 +1,120 @@
 package prj;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-import javafx.scene.layout.Border;
+import com.sun.prism.Graphics;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 
-public class Fenetre extends JFrame {
+public class Fenetre extends JFrame{
+	
+	
+	public Fenetre(){
+		setTitle("4 mots pour 1 mots"); //On donne un titre à l'application
+		setSize(900,500); //On donne une taille à notre fenêtre
+		setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
+		setResizable(false); //On interdit la redimensionnement de la fenêtre
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+		setContentPane(buildContentPane());
+		
+		//String[][] tableau = Tab.afficher();
+		// Tu récupère les 4 premières casess du tableau et tu les stockent dans des String. qui remplaceront "mot 1 etc").
+		
+		//this.setLayout(new BorderLayout());
+		//this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		
+		this.setLayout(null);
+		//this.setLayout(new GridLayout(3, 1));
+		//this.setLayout(new GridLayout(2, 1));
+		//this.setContentPane(new GridLayout(2, 1));
+		
+		JButton b1 = new JButton("Mot 1");
+		JButton b2 = new JButton("Mot 2");
+		JButton b3 = new JButton("Mot 3");
+		JButton b4 = new JButton("Mot 4");
+		
+		b1.setBounds(100, 50, 300, 50);
+		b2.setBounds(500, 50, 300, 50);
+		b3.setBounds(100, 100, 300, 50);
+		b4.setBounds(500, 100, 300, 50);
+		
+		this.getContentPane().add(b1);
+		this.getContentPane().add(b2);
+		this.getContentPane().add(b3);
+		this.getContentPane().add(b4);
+		
+		
+		
+		this.setVisible(true);
+		
+		
+	}
+	
 	
 	private JPanel buildContentPane(){
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout());
-	
-		JLabel label = new JLabel("Bienvenue dans 4 mots pour 1 mot \n");
+		panel.setBackground(Color.white);
 		
-		panel.add(label);
+		
+		JTextField field1;
+		
+		field1 = new JTextField();
+		field1.setColumns(10);
+		
+		field1.setBounds(100, 200, 400, 50);
+		
+		panel.add(field1); // Ajout du textField
+		
+	
+		
+		
+		JButton bouton = new JButton(new CalculAction(this, "Valider !"));
+		bouton.setBounds(550, 200, 150, 50);
+		panel.add(bouton);
+		
+		//JLabel label = new JLabel("Résultat : Pas encore calculé");
+		
+		//panel.add(label);
 		
 		return panel;
-	}	
-
-	public Fenetre(){
-		
-		this.setTitle("4 mots pour 1 mot");
-		this.setSize(700, 700);
-		this.setResizable(true);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setContentPane(buildContentPane());
-		
-		
-		this.setLayout(new GridLayout(2, 2));
-		
-		JButton button1 = new JButton("1");
-		JButton button2 = new JButton("2");
-		JButton button3 = new JButton("3");
-		JButton button4 = new JButton("4");
-		
-		this.getContentPane().add(button1);
-		//button1.setSize(10, 10);
-		button1.setLocation(300, 300);
-	
-		this.getContentPane().add(button2);
-		button2.setSize(10, 10);
-		this.getContentPane().add(button3);
-		button3.setSize(10, 10);
-		this.getContentPane().add(button4);
-		button4.setSize(10, 10);
-		
-		
-		
-
-		this.setVisible(true);
-		
 	}
+	
 
+	public class CalculAction extends AbstractAction {
+		private Fenetre fenetre;
+		
+		public CalculAction(Fenetre fenetre, String texte){
+			super(texte);
+			
+			this.fenetre = fenetre;
+		}
+		
+		public void actionPerformed1(ActionEvent e) { 
+			//Action lors du clic sur le bouton calculer
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		} 
+	}
+	
+
+
+	
+
+	
+	
 }
